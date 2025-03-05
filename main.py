bf028e8f06fafb3e8067f792e435b63c1df66a64
Elisabeth Elgin <eje0004@uah.edu>
2025-03-05 16:55:58 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from homePage import HomePage
+from settingPage import SettingPage
+
+if __name__ == "__main__":
+    root = tk.Tk()  
+    home = HomePage(root)
+    root.mainloop() 
